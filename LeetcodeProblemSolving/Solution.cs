eedf92298eba98d124d7d3676e3c64798f41e98e
YousifAladower <63420481+YousifAladower@users.noreply.github.com>
2024-11-15 23:27:02 +0300

    update

--- LeetcodeProblemSolving/Solution.cs
+++ LeetcodeProblemSolving/Solution.cs
@@ -12,12 +12,11 @@
     }
     public class Solution
     {
-        /*public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
             ListNode dummyHead = new ListNode(0); // Dummy head to simplify handling the result list
             ListNode current = dummyHead;
             int carry = 0;
-
             while (l1 != null || l2 != null || carry != 0)
             {
                 int sum = carry;
@@ -33,15 +32,13 @@
                     sum += l2.val;
                     l2 = l2.next;
                 }
-
                 // Calculate new digit and carry
                 carry = sum / 10;
                 current.next = new ListNode(sum % 10);
                 current = current.next;
             }
-
             return dummyHead.next; // Return the next node to skip the dummy head
-        }*/
+        }
 
         /*        public static int RomanToInt(string s)
                 {
